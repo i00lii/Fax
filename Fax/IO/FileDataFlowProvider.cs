6e84cddccf4bd813ada7494e887442ec3dce7c2a
daniil <daniil@webkontrol.ru>
2018-03-30 20:21:48 +0300

    Added file flow

---
+++ Fax/IO/FileDataFlowProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fax.IO
+{
+	public class FileDataFlowProvider : IDataFlowProvider
+	{
+		private readonly IDataFlowProvider _innerProvider;
+
+		public FileDataFlowProvider( string file )
+			=> _innerProvider
+			= new StreamDataFlowProvider( () => File.Open( file, FileMode.Open, FileAccess.Read, FileShare.Read ) );
+
+		public IDataFlow CreateFlow() => _innerProvider.CreateFlow();
+	}
+}
