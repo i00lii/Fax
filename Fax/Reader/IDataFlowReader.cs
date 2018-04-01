d1bf26c1fc20aab396430ea65e5c6595fff86e59
daniil <daniil@webkontrol.ru>
2018-04-01 16:32:37 +0300

    Added data reader abstractions

---
+++ Fax/Reader/IDataFlowReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fax.IO;
+
+namespace Fax.Reader
+{
+	public interface IDataFlowReader
+	{
+		IEnumerable<TableRow> ReadFlow( IDataFlowProvider flowProvider );
+	}
+}
---
+++ Fax/Reader/TableRow.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fax.Reader
+{
+	public readonly struct TableRow : IEquatable<TableRow>
+	{
+		public TableRow( int rangeSize, double averageUniqueWordsPerRange, int rangesCount )
+		{
+			RangeSize = rangeSize;
+			AverageUniqueWordsPerRange = averageUniqueWordsPerRange;
+			RangesCount = rangesCount;
+		}
+
+		public int RangeSize { get; }
+		public double AverageUniqueWordsPerRange { get; }
+		public int RangesCount { get; }
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				const int prime = -1521134295;
+				int hash = 12345701;
+				hash = hash * prime + EqualityComparer<int>.Default.GetHashCode( RangeSize );
+				hash = hash * prime + EqualityComparer<double>.Default.GetHashCode( AverageUniqueWordsPerRange );
+				hash = hash * prime + EqualityComparer<int>.Default.GetHashCode( RangesCount );
+				return hash;
+			}
+		}
+
+		public bool Equals( TableRow other ) => EqualityComparer<int>.Default.Equals( RangeSize, other.RangeSize ) && AverageUniqueWordsPerRange == other.AverageUniqueWordsPerRange && EqualityComparer<int>.Default.Equals( RangesCount, other.RangesCount );
+		public override bool Equals( object obj ) => obj is Fax.Reader.TableRow other && Equals( other );
+
+		public static bool operator ==( TableRow x, TableRow y ) => x.Equals( y );
+		public static bool operator !=( TableRow x, TableRow y ) => !x.Equals( y );
+	}
+}
